5105f03f4f1abb545e0627937056894dcf1a1d76
Nwafor Emmanuel <emmanuelnwaforce@gmail.com>
2023-04-15 12:34:49 +0100

    first commit

---
+++ src/components/login.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import './login.css';
+
+function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    
+  }
+
+  return (
+    <div className="signin-container">
+      <div className="signin-logo">
+        <h1 className='span'>Enter your email address</h1>
+        <br />
+        <p className='span'>Type your email to log in or create a ComicBay account.</p>
+      </div>
+      <form className="signin-form" onSubmit={handleSubmit}>
+        <div className='form-group'>
+
+          <input
+            type="email" required
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+           
+          />
+          <label>Enter email</label>
+        </div>
+        <br />
+        <div className='form-group'>
+          
+          <input
+            type="password" required 
+            minLength={8}
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+          <label>Enter password</label>
+        </div>
+        <br />
+        <div className='two_buttons'>
+        <button type="submit">Sign In</button>
+    
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default Login;
